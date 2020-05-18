1ab64598003537e801998eea8ffacfc53b1c9a9c
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-05-17 20:18:06 -0700

    working train scheduler

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,4 @@
+//connecting to Firebase database
 var config = {
     apiKey: "AIzaSyDbIxNP0S3PmWWz8uixUUvz7SPExhFtIwI",
     authDomain: "ucla-bootcampyp.firebaseapp.com",
@@ -56,26 +57,46 @@ var config = {
       var newStart = sv.firstTime;
       var newFrequency = sv.frequency;
 
-   //console log new train info
-   console.log(newTrain);
-   console.log(newDestination);
-   console.log(newStart);
-   console.log(newFrequency);
+    //console log new train info
+        console.log(newTrain);
+        console.log(newDestination);
+        console.log(newStart);
+        console.log(newFrequency);
 
-   //variable of next arrival time based on current time and frequency
+   //first Time (modeled from train-example.html)
+   var firstTimeConverted = moment(newStart, "HH:mm").subtract(1, "years");
+   var currentTime = moment();
 
+   //difference between current time and next arrival
+   var timeDifference = moment().diff(moment(firstTimeConverted), "minutes");
 
-   //variable finding distance between current time and next arrival for "minutes away"
+   //minutes away till the next train
+   var nextTrain = moment().add(minutesAway, "minutes");
+   
+   //time remainder using difference of times and the frequency. remainder is used for minutes away calculation
+   var tRemainder = timeDifference % newFrequency;
 
+   // minuites until next train arrives
+   var minutesAway = newFrequency - tRemainder;
 
+    //console log time information
+        console.log(firstTimeConverted);
+        console.log(moment(currentTime).format("hh:mm"));
+        console.log(timeDifference);
+        console.log(tRemainder);
+        console.log(minutesAway);
+        console.log(moment(nextTrain).format("hh:mm"));
+
+     //appending new train information to the current tran schedule card   
     var addRow = $("<tr>").append(
         $("<td>").text(newTrain),
         $("<td>").text(newDestination),
         $("<td>").text(newFrequency),
+        $("<td>").text(moment(nextTrain).format("hh:mm")),
+        $("<td>").text(tMinutesTillTrain),
 
     );
 
     $("table tbody").append(addRow);
 
-
-  })
+  });
