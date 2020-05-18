10d8628debf37116f2840865b901a00a57878a91
Yssabel Pangilinan <yssabelpangilinan@yahoo.com>
2020-05-17 19:21:48 -0700

    initial edits to js adding firebase

---
+++ assets/app.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyDbIxNP0S3PmWWz8uixUUvz7SPExhFtIwI",
+    authDomain: "ucla-bootcampyp.firebaseapp.com",
+    databaseURL: "https://ucla-bootcampyp.firebaseio.com",
+    projectId: "ucla-bootcampyp",
+    storageBucket: "ucla-bootcampyp.appspot.com",
+    messagingSenderId: "684338918033",
+    appId: "1:684338918033:web:fdaf759ed3ad9dd83e4530",
+    measurementId: "G-RLCK0YMRTX"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(config);
